b579d17d7adbcca3cf080ab9191855d23b0c00b9
BalameenaN <balameena.navukkarasu@gmail.com>
2025-01-26 21:48:01 -0700

    path file changes

--- paths/user.js
+++ paths/user.js
@@ -8,6 +8,25 @@ router.get("/user",(req,res)=>{
     console.log("inside /user");
     res.json(users);
 })
+.post("/user",(req,res)=>{
+    console.log("inside / POST");
+    if(req.body.name && req.body.email && req.body.userName && req.body.phoneNo){
+        if (users.find((u) => u.username == req.body.username)) {
+            next(error(409, "Username Already Taken"));
+          }
+          let length = users.length;
+          const user=  {
+            id: length+1,
+            name: req.body.name,
+            email: req.body.email,
+            phoneNo: req.body.phoneNo,
+            userName:  req.body.userName
+          }
+          
+          users.push(user);
+          res.json(users[users.length-1]);
+    }else next(error(400, "Insufficient Data"));
+})
 
 router.get("/user/:id",(req,res,next)=>{
     console.log("/user/id");
@@ -20,6 +39,7 @@ router.get("/user/:id",(req,res,next)=>{
     }
     else next(error(404,"User Data not found"));
 })
+
 
 
 
